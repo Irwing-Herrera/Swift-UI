eda3f6d78c48c4f5d3509abf2ba54c10a2ae2244
Irwing Josue Herrera Cuautle <c330593@santander.com.mx>
2022-05-11 12:26:29 -0500

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by MacBook on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
